044b7916158d213ad2811ba1aa1e390fabd434e8
Ishann0109 <165162373+Ishann0109@users.noreply.github.com>
2024-07-31 18:10:36 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+// to run the app use npm run dev in the terminal not npm start.
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
